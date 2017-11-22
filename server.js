01e0ae4fc1e14b6a5150db771c97eca6b8f68172
Iris <ischolten.is@gmail.com>
2017-11-22 00:01:49 -0500

    moved status 400 down so it didnt do it for every request

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ var express = require('express');
 var app = express();
 var port = process.env.Port || 3000;
 var mongoose = require('mongoose');
-var Task = require('./api/models/jobQueueModel');
+var Task = require('./api/models/jobqueueModel');
 var bodyParser = require('body-parser');
 
 mongoose.Promise = global.Promise;
@@ -10,9 +10,7 @@ mongoose.connect('mongodb://localhost/Jobqueuedb');
 
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
-app.use(function(req, res) {
-  res.status(404).send({url: req.originalUrl + ' not found'})
-});
+
 
 var routes = require('./api/routes/jobqueueRoutes');
 routes(app);
@@ -21,3 +19,6 @@ app.listen(port);
 
 console.log('job queue rest api server started on:' + port);
 
+app.use(function(req, res) {
+  res.status(404).send({url: req.originalUrl + ' not found'})
+});
